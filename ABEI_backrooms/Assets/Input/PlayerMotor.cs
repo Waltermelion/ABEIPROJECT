8460123cf5f9c4324a05a49937e25bf73b7a19f2
Waltermelion <guilhermelpmeneses@gmail.com>
2022-06-10 23:15:46 +0100

    Added entity mesh, no animation
     further development needed

--- ABEI_backrooms/Assets/Input/PlayerMotor.cs
+++ ABEI_backrooms/Assets/Input/PlayerMotor.cs
@@ -8,23 +8,34 @@ public class PlayerMotor : MonoBehaviour
     private Vector3 playerVelocity;
     public float speed = 5f;
     
-    // Start is called before the first frame update
+    [Header("Audio")] 
+    public AudioClip[] footStepSound;
+    public AudioClip jumpSound;
+    public AudioSource audioSource1;
     void Start()
     {
         controller = GetComponent<CharacterController>(); 
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     public void ProcessMove(Vector3 input)
     {
         Vector3 moveDirection = Vector3.zero;
         moveDirection.x = input.x;
         moveDirection.z = input.y;
         controller.Move(transform.TransformDirection(moveDirection) * speed * Time.deltaTime);
+    }
+
+    void PlayFootStepAudio()
+    {
+        if (!controller.isGrounded)
+        {
+            return;
+        }
+
+        int n = Random.Range(1, footStepSound.Length);
+        audioSource1.clip = footStepSound[n];
+        audioSource1.PlayOneShot(audioSource1.clip);
+
+        footStepSound[n] = footStepSound[0];
+        footStepSound[0] = audioSource1.clip;
     }
 }
