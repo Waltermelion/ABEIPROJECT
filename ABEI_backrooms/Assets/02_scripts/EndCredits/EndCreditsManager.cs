2bb0b4256b8446f724ab7073d6ffb6ac9feb6a7f
Waltermelion <guilhermelpmeneses@gmail.com>
2022-06-22 23:44:11 +0100

    done idk i don't remember

--- ABEI_backrooms/Assets/02_scripts/EndCredits/EndCreditsManager.cs
+++ ABEI_backrooms/Assets/02_scripts/EndCredits/EndCreditsManager.cs
@@ -4,6 +4,13 @@ using UnityEngine.SceneManagement;
 public class EndCreditsManager : MonoBehaviour
 {
     [SerializeField] private GameObject backButton;
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+
     public void BackToMainMenu()
     {
         SceneManager.LoadScene("MainMenu");
